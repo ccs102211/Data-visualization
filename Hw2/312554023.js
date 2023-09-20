3aceaa711bb55ebd7d32f96526526d83d1959ae0
unknown <ccs102211@gmail.com>
2023-09-21 01:20:07 +0800

    hw1

--- Hw2/312554023.js
+++ Hw2/312554023.js
@@ -4,134 +4,168 @@ const colorMapping = {
   'Iris-virginica': 'green'
 };
 
-(function(d3) {
+(function (d3) {
   'use strict';
 
   const dropdownMenu = (selection, props) => {
-      const {
-          options,
-          onOptionClicked,
-          selectedOptions
-      } = props;
+    const {
+      options,
+      onOptionClicked,
+      selectedOption,
+      menuId
+    } = props;
 
-      let select = selection.selectAll('select').data([null]);
-      select = select.enter().append('select')
-          .merge(select)
-          .on('change', function() {
-              onOptionClicked(this.value);
-          });
+    let select = selection.selectAll(`#${menuId} select`).data([null]);
+    select = select.enter().append('select')
+      .merge(select)
+      .on('change', function() {
+          onOptionClicked(this.value, menuId);
+          if (this.value !== `-- select ${menuId.split('-').pop()} dimension --`) {
+              d3.select(this).selectAll('option[value=""]').remove();
+          }
+      });
 
-      const option = select.selectAll('option').data(options);
-      option.enter().append('option')
-          .attr('value', d => d)
-          .property('selected', d => selectedOptions.includes(d))
-          .text(d => d);
-      option.exit().remove();
-      select.selectAll('option').filter(d => d === "class").remove();
+    select.selectAll('option:not(:first-child)').remove();
+
+    const option = select.selectAll('option').data(options, d => d);
+    option.enter().append('option')
+      .attr('value', d => d)
+      .property('selected', d => d === selectedOption)
+      .text(d => d);
+    select.selectAll('option').filter(d => d === "class").remove();
   };
 
   const parallelCoordinatePlot = (selection, props) => {
-      const {
-          dimensions,
-          margin,
-          width,
-          height,
-          data
-      } = props;
+    const {
+        dimensions,
+        width,
+        height,
+        data
+    } = props;
 
-      const innerWidth = width - margin.left - margin.right;
-      const innerHeight = height - margin.top - margin.bottom;
+    const margin = { top: 30, right: 10, bottom: 10, left: 10 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
-      const x = d3.scalePoint()
-          .domain(dimensions)
-          .range([margin.left, width - margin.right]);
+    const y = {};
 
-      const y = {};
-      dimensions.forEach(dim => {
-          y[dim] = d3.scaleLinear()
-              .domain(d3.extent(data, d => d[dim]))
-              .range([innerHeight + margin.top, margin.top])
-              .nice();
+    const dimensionNames = Object.values(dimensions).map((d, index) => {
+        return Object.values(dimensions).slice(0, index + 1).filter(val => val === d).length > 1 ? d + index : d;
+    });
+
+    dimensionNames.forEach((dimension, index) => {
+        y[dimension] = d3.scaleLinear()
+            .domain(d3.extent(data, d => d[dimension.replace(/[0-9]/g, "")]))
+            .range([innerHeight, 0]);
+    });
+
+    const path = d => {
+      const coordinates = dimensionNames.map((p, index) => {
+          const value = d[p.replace(/[0-9]/g, "")];
+          return [x(p), y[p](value)];
       });
-
-      const line = d3.line()
-          .defined(d => !isNaN(d[1]))
-          .x(d => d[0])
-          .y(d => d[1]);
-
-      const g = selection.selectAll('.container').data([null]);
-      const gEnter = g.enter().append('g')
-          .attr('class', 'container')
-          .attr('transform', `translate(${margin.left},${margin.top})`);
-      gEnter.merge(g);
-
-      dimensions.forEach(dim => {
-          const axis = d3.axisLeft(y[dim]);
-          gEnter.append('g')
-              .attr('class', `y-axis ${dim}`)
-              .attr('transform', `translate(${x(dim)},0)`)
-              .call(axis);
-      });
-
-      const paths = gEnter.selectAll('.path').data(data);
-      paths.enter().append('path')
-          .attr('class', 'path')
-          .attr('d', d => line(dimensions.map(dim => [x(dim), y[dim](d[dim])])))
-          .style('fill', 'none')
-          .style('stroke', d => colorMapping[d.class])
-          .style('opacity', 0.5);
-
+      if (coordinates.length > 0) {
+          // Take the first coordinate as the starting point with 'M'
+          const start = `M${coordinates[0].join(",")}`;
+          // For the rest of the coordinates, use 'L'
+          const lines = coordinates.slice(1).map(coord => `L${coord.join(",")}`).join("");
+          return start + lines;
+      }
+      return "";  // Return an empty string if there are no coordinates
   };
+
+    const x = d3.scalePoint()
+        .range([0, innerWidth])
+        .padding(1)
+        .domain(dimensionNames);
+
+    const line = d3.line();
+
+    const background = selection.append("g")
+        .attr("class", "background")
+        .selectAll("path")
+        .data(data)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("stroke", "#ccc")
+        .attr("fill", "none");
+
+    const foreground = selection.append("g")
+        .attr("class", "foreground")
+        .selectAll("path")
+        .data(data)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("stroke", d => colorMapping[d.class])
+        .attr("fill", "none");
+
+    const dimension = selection.selectAll(".dimension")
+        .data(dimensionNames)
+        .enter().append("g")
+        .attr("class", "dimension")
+        .attr("transform", d => `translate(${x(d)})`);
+
+    dimension.append("g")
+        .attr("class", "axis")
+        .each(function(d) { d3.select(this).call(d3.axisLeft(y[d])); })
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(d => d);
+
+    // Add brushing functionality here if needed
+};
+
 
   const svg = d3.select('svg');
   const width = +svg.attr('width');
   const height = +svg.attr('height');
 
   let data;
-  let selectedDimensions = [];
+  let dimensions = {};
 
-  const onDimensionSelected = dimension => {
-      if (!selectedDimensions.includes(dimension)) {
-          selectedDimensions.push(dimension);
-      }
-      if (selectedDimensions.length > 4) {
-          selectedDimensions.shift();
-      }
-      render();
-  };
+  const onDimensionSelected = (dimension, menuId) => {
+    dimensions[menuId] = dimension;
+
+    // Clear the SVG content
+    svg.selectAll("*").remove();
+
+    // Then re-render the chart
+    render();
+};
 
   const render = () => {
-      d3.select('#dimension-menu')
-          .call(dropdownMenu, {
-              options: data.columns,
-              onOptionClicked: onDimensionSelected,
-              selectedOptions: selectedDimensions
-          });
+    svg.selectAll("*").remove();
 
-      svg.call(parallelCoordinatePlot, {
-          dimensions: selectedDimensions,
-          margin: {
-              top: 20,
-              right: 20,
-              bottom: 20,
-              left: 20
-          },
-          width,
-          height,
-          data
-      });
+    const menus = ["dimension-menu-1", "dimension-menu-2", "dimension-menu-3", "dimension-menu-4"];
+    menus.forEach(menuId => {
+      d3.select(`#${menuId}`)
+        .call(dropdownMenu, {
+          options: data.columns,
+          onOptionClicked: onDimensionSelected,
+          selectedOption: dimensions[menuId] || null,
+          menuId: menuId
+        });
+    });
+    
+    svg.call(parallelCoordinatePlot, {
+      dimensions,
+      width,
+      height,
+      data
+    });
   };
 
   d3.csv('http://vis.lab.djosix.com:2023/data/iris.csv')
-      .then(irisdata => {
-          data = irisdata;
-          data.forEach(d => {
-              d['petal length'] = +d['petal length'];
-              d['petal width'] = +d['petal width'];
-              d['sepal length'] = +d['sepal length'];
-              d['sepal width'] = +d['sepal width'];
-          });
-          render();
+    .then(irisdata => {
+      data = irisdata;
+      data.forEach(d => {
+        d['petal length'] = +d['petal length'];
+        d['petal width'] = +d['petal width'];
+        d['sepal length'] = +d['sepal length'];
+        d['sepal width'] = +d['sepal width'];
       });
+      render();
+    });
 
 }(d3));
