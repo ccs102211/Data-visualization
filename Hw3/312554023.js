84c90f48519b1e01d1e6ac13bdff2ab11439f2e1
unknown <ccs102211@gmail.com>
2023-10-04 13:39:17 +0800

    Hw3

--- Hw3/312554023.js
+++ Hw3/312554023.js
@@ -3,37 +3,29 @@ fetch('abalone.data')
   .then(data => {
     const rows = data.split('\n').filter(line => line.trim() !== '').map(line => line.split(','));
 
-    const featureMap = {
-      Sex: 0,
-      Length: 1,
-      Diameter: 2,
-      Height: 3,
-      Whole_weight: 4,
-      Shucked_weight: 5,
-      Viscera_weight: 6,
-      Shell_weight: 7,
-      Rings: 8
-    };
+    const features = ['Sex', 'Length', 'Diameter', 'Height', 'Whole_weight', 'Shucked_weight', 'Viscera_weight', 'Shell_weight', 'Rings'];
 
-    const numericalSex = rows.map(row => {
-      switch (row[featureMap.Sex]) {
-        case 'M': return 1;
-        case 'F': return 2;
-        case 'I': return 3;
-        default: return null;
-      }
-    });
+    const sexValues = ['M', 'F', 'I']; // 使用 I 代表 infant
 
-    let correlations = {};
-    for (let feature in featureMap) {
-      if (feature !== 'Sex') {
-        const featureValues = rows.map(row => parseFloat(row[featureMap[feature]]));
-        const correlation = calculatePearsonCorrelation(numericalSex, featureValues);
-        correlations[feature] = correlation;
+    let allCorrelationMatrices = {};
+
+    for (let sex of sexValues) {
+      const filteredRows = rows.filter(row => row[0] === sex);
+      let matrix = [];
+      for (let i = 1; i < features.length; i++) {
+        let row = [];
+        for (let j = 1; j < features.length; j++) {
+          const values1 = filteredRows.map(row => parseFloat(row[i]));
+          const values2 = filteredRows.map(row => parseFloat(row[j]));
+          const correlation = i === j ? 1 : calculatePearsonCorrelation(values1, values2);  // 對角線上的值應該是1
+          row.push(correlation);
+        }
+        matrix.push(row);
       }
+      allCorrelationMatrices[sex] = matrix;
     }
 
-    document.getElementById('results').textContent = JSON.stringify(correlations, null, 2);
+    document.getElementById('results').textContent = JSON.stringify(allCorrelationMatrices, null, 2);
   })
   .catch(error => console.error("Error:", error));
 
@@ -53,7 +45,37 @@ function calculatePearsonCorrelation(arr1, arr2) {
 
   return numerator / (Math.sqrt(denominator1) * Math.sqrt(denominator2));
 }
+drawHeatmap(allCorrelationMatrices['M'], features);
 
-function mean(arr) {
-  return arr.reduce((sum, val) => sum + val, 0) / arr.length;
+function drawHeatmap(matrix, features) {
+  const size = 50;  // 每個格子的大小
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+  const width = matrix[0].length * size + margin.left + margin.right;
+  const height = matrix.length * size + margin.top + margin.bottom;
+
+  // 創建SVG元素
+  const svg = d3.select("#heatmap").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // 定義色彩比例尺
+  const colors = d3.scaleSequential(d3.interpolateCool)
+    .domain([-1, 1]); // Pearson correlation範圍是[-1, 1]
+
+  // 繪製熱圖
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      svg.append("rect")
+        .attr("x", j * size)
+        .attr("y", i * size)
+        .attr("width", size)
+        .attr("height", size)
+        .style("fill", colors(matrix[i][j]))
+        .style("stroke", "white");
+    }
+  }
 }
+
+
