d461e4d8645d8b17b5b7173ff2a8634781e7e4a2
unknown <ccs102211@gmail.com>
2023-10-05 17:02:01 +0800

    hw3

--- Hw3/312554023.js
+++ Hw3/312554023.js
@@ -1,18 +1,18 @@
 const colorSchemes = {
-  'M': d3.scaleSequential(d3.interpolateBlues),  // 代表從淺藍色到深藍色
-  'F': d3.scaleSequential(d3.interpolateReds),   // 代表從淺紅色到深紅色
-  'I': d3.scaleSequential(d3.interpolateGreens)  // 代表從淺綠色到深綠色
+  'M': d3.scaleSequential(d3.interpolateBlues),
+  'F': d3.scaleSequential(d3.interpolateReds),
+  'I': d3.scaleSequential(d3.interpolateGreens)
 };
 
 
-fetch('abalone.data')
+fetch('http://vis.lab.djosix.com:2023/data/abalone.data')
   .then(response => response.text())
   .then(data => {
     const rows = data.split('\n').filter(line => line.trim() !== '').map(line => line.split(','));
 
     const features = ['Sex', 'Length', 'Diameter', 'Height', 'Whole_weight', 'Shucked_weight', 'Viscera_weight', 'Shell_weight', 'Rings'];
 
-    const sexValues = ['M', 'F', 'I']; // 使用 I 代表 infant
+    const sexValues = ['M', 'F', 'I'];
 
     let allCorrelationMatrices = {};
 
@@ -24,7 +24,7 @@ fetch('abalone.data')
         for (let j = 1; j < features.length; j++) {
           const values1 = filteredRows.map(row => parseFloat(row[i]));
           const values2 = filteredRows.map(row => parseFloat(row[j]));
-          const correlation = i === j ? 1 : calculatePearsonCorrelation(values1, values2);  // 對角線上的值應該是1
+          const correlation = i === j ? 1 : calculatePearsonCorrelation(values1, values2);
           row.push(correlation);
         }
         matrix.push(row);
@@ -61,7 +61,7 @@ function mean(arr) {
 
 function drawHeatmap(matrix, features, sex) {
   const containerId = `#heatmap-${sex}`;
-  const size = 60;  // 每個格子的大小
+  const size = 60;
   const margin = { top: 100, right: 150, bottom: 50, left: 150 };
   const width = matrix[0].length * size + margin.left + margin.right;
   const height = matrix.length * size + margin.top + margin.bottom;
@@ -75,10 +75,10 @@ function drawHeatmap(matrix, features, sex) {
 
   const rectGroup = svg.append("g");
   const textGroup = svg.append("g");
-  // 定義色彩比例尺
+
   const colors = colorSchemes[sex].domain([-1, 1]);
 
-  // 繪製熱圖
+
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       rectGroup.append("rect")
@@ -86,78 +86,77 @@ function drawHeatmap(matrix, features, sex) {
         .attr("y", i * size)
         .attr("width", size)
         .attr("height", size)
-        .style("fill", colors(matrix[i][j]))  // 使用定義的色彩方案
+        .style("fill", colors(matrix[i][j]))
         .style("stroke", "white")
         .on('mouseover', function () {
           d3.select(this)
-            .style('opacity', '0.7')  // 減少透明度
-            .attr('width', size * 1.2)  // 讓格子稍微放大
+            .style('opacity', '0.7')
+            .attr('width', size * 1.2)
             .attr('height', size * 1.2)
-            .attr('x', j * size - (size * 0.05))  // 調整位置以確保格子在中心放大
+            .attr('x', j * size - (size * 0.05))
             .attr('y', i * size - (size * 0.05));
 
           d3.select(`#text-${i}-${j}`)
-            .attr("font-size", "18px")  // 假設原始大小為10px，現在將其增加到12px
-            .attr("x", j * size + size * 1.2 / 2)  // 調整位置以確保數字在放大的格子中居中
+            .attr("font-size", "18px")
+            .attr("x", j * size + size * 1.2 / 2)
             .attr("y", i * size + size * 1.2 / 2);
         })
         .on('mouseout', function () {
           d3.select(this)
-            .style('opacity', '1')  // 恢復透明度
-            .attr('width', size)  // 恢復到原始大小
+            .style('opacity', '1')
+            .attr('width', size)
             .attr('height', size)
             .attr('x', j * size)
             .attr('y', i * size);
 
           d3.select(`#text-${i}-${j}`)
             .attr("font-size", "15px")
-            .attr("x", j * size + size / 2)  // 恢復到原始位置
+            .attr("x", j * size + size / 2)
             .attr("y", i * size + size / 2);
         });
 
-      // 在每個格子中填充數字
       textGroup.append("text")
-        .attr("x", j * size + size / 2)  // 使數字在格子中居中
+        .attr("x", j * size + size / 2)
         .attr("y", i * size + size / 2)
-        .attr("dy", ".35em")  // 微調，使文字垂直居中
-        .attr("text-anchor", "middle")  // 文字居中對齊
-        .attr("id", `text-${i}-${j}`) // 添加這個ID
-        .text(matrix[i][j].toFixed(2))  // 保留兩位小數
-        .attr("font-size", "15px")  // 可根據需要調整字體大小
-        .attr("fill", "black");  // 文字顏色
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr("id", `text-${i}-${j}`)
+        .text(matrix[i][j].toFixed(2))
+        .attr("font-size", "15px")
+        .attr("fill", "black");
 
       textGroup.append("text")
-        .attr("x", width / 2 - margin.left) // center the text
-        .attr("y", -80)  // move the text above the heatmap
+        .attr("x", width / 2 - margin.left)
+        .attr("y", -80)
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("font-weight", "bold")
         .text(`Abalone Type: ${sex}`);
 
-      // 添加軸標籤
-      const featureLabels = features.slice(1);  // 去除'Sex'，因為它不是一個特徵
-      // Y軸標籤
+
+      const featureLabels = features.slice(1);
+
       svg.selectAll(".yLabel")
         .data(featureLabels)
         .enter()
         .append("text")
         .text(d => d)
-        .attr("x", -10) // slight padding on the left
-        .attr("y", (d, i) => i * size + size / 2) // centralizing the label with the row
-        .attr("dy", ".35em") // further centering on y-axis
+        .attr("x", -10)
+        .attr("y", (d, i) => i * size + size / 2)
+        .attr("dy", ".35em")
         .attr("text-anchor", "end")
         .attr("font-size", "14px");
 
-      // X軸標籤
+
       svg.selectAll(".xLabel")
         .data(featureLabels)
         .enter()
         .append("text")
         .text(d => d)
-        .attr("x", (d, i) => i * size + size / 2) // centralizing the label with the column
-        .attr("y", -10) // slight padding from the top
+        .attr("x", (d, i) => i * size + size / 2)
+        .attr("y", -10)
         .attr("text-anchor", "middle")
-        .attr("transform", (d, i) => `translate(0,-10) rotate(-25, ${i * size + size / 2}, -10)`)  // rotating around the text starting point
+        .attr("transform", (d, i) => `translate(0,-10) rotate(-25, ${i * size + size / 2}, -10)`)
         .attr("font-size", "14px");
 
     }
